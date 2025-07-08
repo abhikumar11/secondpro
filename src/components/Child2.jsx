34b92e07fe3788597549e6e179800d5acb2febc0
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-07-08 13:28:59 +0530

    initial commit -props

--- src/components/Child2.jsx
+++ src/components/Child2.jsx
@@ -1,5 +1,5 @@
-export const Child2 = (props) => {
-     let { contact, city } = props;
+export const Child2 = ({contact,city}) => {
+    //  let { contact, city } = props;
      return (
           <div>
                <h1 style={{ backgroundColor: "yellow" }}>Child2</h1>
